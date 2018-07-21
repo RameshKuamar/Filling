ddbd7c0511792d9f54466f274c0f0e702dc1d927
RameshKuamar <41305105+RameshKuamar@users.noreply.github.com>
2018-07-21 09:47:50 +0500

    Filinng app

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            this.textBox3.ScrollBars = ScrollBars.Both;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(textBox1.Text + textBox2.Text);
+            sw.Write(this.textBox3.Text);
+            MessageBox.Show("File write");
+            sw.Close();
+        }
+    }
+}
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Select Extension";
+            string[] extension = {".txt"};
+            comboBox1.Items.AddRange(extension);
+            this.textBox3.ScrollBars = ScrollBars.Both;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(textBox1.Text + textBox2.Text+comboBox1.Text);
+            textBox3.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+    }
+}
